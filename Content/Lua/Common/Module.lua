e29e7d34b2884d971981a4b56c2fd0f60be01f38
crazytuzi <liuxiangcode@qq.com>
2020-07-11 16:44:04 +0800

    Stack

--- Content/Lua/Common/Module.lua
+++ Content/Lua/Common/Module.lua
@@ -1,1 +1,2 @@
 require "Common/Functions"
+_G.Stack = require "Common/Stack"
---
+++ Content/Lua/Common/Stack.lua
@@ -0,0 +1,52 @@
+local Stack = _G.Class("Stack")
+
+local function __init(self)
+    self._data = {}
+end
+
+local function Push(self, Element)
+    if Element == nil then
+        _G.Logger.warn("Stack:Push => Element is not valid")
+        return
+    end
+
+    table.insert(self._data, Element)
+end
+
+local function Pop(self)
+    if self:IsEmpty() then
+        return nil
+    end
+
+    table.remove(self._data, self:Num())
+end
+
+local function Empty(self)
+    self._data = {}
+end
+
+local function Top(self)
+    if self:IsEmpty() then
+        return nil
+    end
+
+    return self._data[self:Num()]
+end
+
+local function Num(self)
+    return #self._data
+end
+
+local function IsEmpty(self)
+    return self:Num() == 0
+end
+
+Stack.__init = __init
+Stack.Push = Push
+Stack.Pop = Pop
+Stack.Empty = Empty
+Stack.Top = Top
+Stack.Num = Num
+Stack.IsEmpty = IsEmpty
+
+return Stack
