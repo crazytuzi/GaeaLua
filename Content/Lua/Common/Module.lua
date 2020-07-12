6670a4cdd91108e5e35e3f417515acf1a56b1a3a
crazytuzi <liuxiangcode@qq.com>
2020-07-12 21:57:37 +0800

    Pool

--- Content/Lua/Common/Module.lua
+++ Content/Lua/Common/Module.lua
@@ -2,3 +2,4 @@ require "Common/Functions"
 _G.Stack = require "Common/Stack"
 _G.Queue = require "Common/Queue"
 _G.Heap = require "Common/Heap"
+_G.Pool, _G.PoolItem = table.unpack(require "Common/Pool")
---
+++ Content/Lua/Common/Pool.lua
@@ -0,0 +1,81 @@
+local Pool = _G.Class("Pool")
+
+local PoolItem = _G.Class("PoolItem")
+
+local function __init(self, ClassType, Capacity)
+    if ClassType == nil or ClassType.super ~= PoolItem then
+        _G.Logger.warn("Pool.__init => ClassType is not valid")
+        return
+    end
+
+    if Capacity == nil or Capacity <= 0 then
+        Capacity = 10
+    end
+
+    self._pool = _G.Queue.New(Capacity)
+
+    self._class = ClassType
+
+    self._capacity = Capacity
+end
+
+local function IsEmpty(self)
+    return self._pool:IsEmpty()
+end
+
+local function Num(self)
+    return self._pool:Num()
+end
+
+local function IsFull(self)
+    return Num(self) >= self._capacity
+end
+
+local function Push(self, Element)
+    if Element == nil then
+        _G.Logger.warn("Pool.Push => Element is not valid")
+        return
+    end
+
+    if Element._class_type ~= self._class then
+        _G.Logger.warn("Pool.Push => Element type is not match")
+        return
+    end
+
+    if IsFull(self) then
+        _G.Logger.warn("Pool.Push => Pool is already full")
+    else
+        self._pool:Enqueue(Element)
+    end
+end
+
+local function Pop(self, ...)
+    local Element
+
+    if IsEmpty(self) then
+        Element = self._class.New(...)
+    else
+        Element = self._pool:Peek()
+
+        self._pool:Dequeue()
+    end
+
+    return Element
+end
+
+local function Empty(self)
+    self._pool:Empty()
+
+    self._pool = nil
+
+    self._class = nil
+
+    self._capacity = nil
+end
+
+Pool.__init = __init
+Pool.Push = Push
+Pool.Pop = Pop
+Pool.Empty = Empty
+
+return table.pack(Pool, PoolItem)
