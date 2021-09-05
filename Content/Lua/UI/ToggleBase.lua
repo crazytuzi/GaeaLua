a6a8d47a47b90a681f7df037418efc20679ed925
Doraemon <liuxiangcode@qq.com>
2021-09-05 16:35:51 +0800

    ToggleGroup

---
+++ Content/Lua/UI/ToggleBase.lua
@@ -0,0 +1,86 @@
+local ToggleBase = _G.Class("ToggleBase")
+
+local function __init(self, Params)
+    local Root, Object, Data = table.unpack(Params)
+
+    self._root = Root
+
+    self._object = Object
+
+    self._data = Data
+
+    self._group = nil
+
+    self._isOn = true
+end
+
+local function OnOperate()
+end
+
+local function OnValueChanged(self)
+end
+
+local function GetIsOn(self)
+    return self._isOn
+end
+
+local function SetIsOn(self, IsOn)
+    if self:GetIsOn() ~= IsOn then
+        self._isOn = IsOn
+
+        self:OnValueChanged()
+    end
+end
+
+local function GetIndex(self)
+    return self._index
+end
+
+local function GetRoot(self)
+    return self._root
+end
+
+local function GetObject(self)
+    return self._object
+end
+
+local function GetData(self)
+    return self._data
+end
+
+local function GetGroup(self)
+    return self._group
+end
+
+local function OnToggleRegister(self)
+end
+
+local function ToggleRegister(self, Params)
+    self._group, self._index = table.unpack(Params)
+
+    self:OnToggleRegister()
+end
+
+local function __delete(self)
+    self._root = nil
+
+    self._object = nil
+
+    self._group = nil
+end
+
+ToggleBase.__init = __init
+ToggleBase.OnOperate = OnOperate
+ToggleBase.OnValueChanged = OnValueChanged
+ToggleBase.GetIsOn = GetIsOn
+ToggleBase.SetIsOn = SetIsOn
+ToggleBase.GetIndex = GetIndex
+ToggleBase.GetRoot = GetRoot
+ToggleBase.GetObject = GetObject
+ToggleBase.GetData = GetData
+ToggleBase.GetGroup = GetGroup
+ToggleBase.OnToggleRegister = OnToggleRegister
+ToggleBase.ToggleRegister = ToggleRegister
+ToggleBase.__delete = __delete
+
+return ToggleBase
---
+++ Content/Lua/UI/ToggleGroupBase.lua
@@ -0,0 +1,197 @@
+local ToggleGroupBase = _G.Class("ToggleGroupBase")
+
+local UButton = _G.import("Button")
+
+local UImage = _G.import("Image")
+
+local UCheckBox = _G.import("CheckBox")
+
+local function __init(self, Params)
+    local MinOn, MaxOn = table.unpack(Params)
+
+    self._minOn = MinOn or 1
+
+    self._maxOn = MaxOn or 1
+
+    self._toggles = {}
+
+    self._activeToggles = {}
+
+    self._delegates = {}
+end
+
+local function GetToggle(self, Index)
+    return self._toggles[Index]
+end
+
+local function HasToggle(self, Index)
+    return GetToggle(self, Index) ~= nil
+end
+
+local function GetActiveToggleKey(self, Index)
+    for key, value in pairs(self._activeToggles) do
+        if value == Index then
+            return key
+        end
+    end
+
+    return nil
+end
+
+local function GetActiveToggle(self, Index)
+    return self._activeToggles[Index]
+end
+
+local function AddActiveToggle(self, Index)
+    table.insert(self._activeToggles, Index)
+end
+
+local function RemoveActiveToggle(self, Index)
+    table.remove(self._activeToggles, Index)
+end
+
+local function IsToggleActive(self, Index)
+    return GetActiveToggle(self, Index) ~= nil
+end
+
+local function GetActiveToggleNum(self)
+    return #self._activeToggles
+end
+
+local function ActiveToggleImplementation(self, Index)
+    if self._maxOn == 0 then
+        return
+    end
+
+    if self._maxOn == 1 then
+        local PreToggle = GetToggle(self, GetActiveToggle(self, 1))
+
+        if PreToggle then
+            PreToggle:SetIsOn(false)
+
+            RemoveActiveToggle(self, 1)
+        end
+    else
+        if self:GetActiveToggleNum() == self._maxOn then
+            return
+        end
+    end
+
+    local Toggle = GetToggle(self, Index)
+
+    if Toggle then
+        AddActiveToggle(self, Index)
+
+        Toggle:SetIsOn(true)
+    end
+end
+
+local function InactiveToggleImplementation(self, Index)
+    if self:GetActiveToggleNum() - 1 < self._minOn then
+        return
+    end
+
+    RemoveActiveToggle(self, GetActiveToggleKey(self, Index))
+
+    local Toggle = GetToggle(self, Index)
+
+    if Toggle then
+        Toggle:SetIsOn(false)
+    end
+end
+
+local function OperateToggleImplementation(self, Index)
+    if IsToggleActive(self, GetActiveToggleKey(self, Index)) then
+        InactiveToggleImplementation(self, Index)
+    else
+        ActiveToggleImplementation(self, Index)
+    end
+end
+
+local function OperateToggle(self, Index)
+    if not HasToggle(self, Index) then
+        return
+    end
+
+    OperateToggleImplementation(self, Index)
+end
+
+local function Operate(self, Toggle)
+    Toggle:OnOperate()
+
+    OperateToggle(self, Toggle:GetIndex())
+end
+
+local function RegisterToggle(self, Toggle)
+    local Object = Toggle:GetObject()
+
+    local EventName
+
+    local Fun
+
+    if Object:IsA(UButton) then
+        EventName = _G.EWidgetEvent.Button.OnClicked
+
+        Fun = function(this, _Toggle)
+            Operate(this, _Toggle)
+        end
+    elseif Object:IsA(UImage) then
+        EventName = _G.EWidgetEvent.Image.OnMouseButtonDown
+
+        Fun = function(this, _, _, _Toggle)
+            Operate(this, _Toggle)
+        end
+    elseif Object:IsA(UCheckBox) then
+        EventName = _G.EWidgetEvent.CheckBox.OnCheckStateChanged
+
+        Fun = function(this, _, _Toggle)
+            Operate(this, _Toggle)
+        end
+    end
+
+    local Delegate = _G.EventHelper.Add(Object, EventName, Fun, self, Toggle)
+
+    table.insert(self._delegates, {Object = Object, EventName = EventName, Delegate = Delegate})
+
+    table.insert(self._toggles, Toggle)
+
+    Toggle:ToggleRegister({self, #self._toggles})
+
+    Toggle:SetIsOn(false)
+end
+
+local function GetToggles(self)
+    return self._toggles
+end
+
+local function GetActiveToggles(self)
+    local Toggles = {}
+
+    for _, value in pairs(self._activeToggles) do
+        table.insert(Toggles, self._toggles[value])
+    end
+
+    return Toggles
+end
+
+local function __delete(self)
+    for _, value in pairs(self._delegates) do
+        _G.EventHelper.Remove(value.Object, value.EventName, value.Delegate)
+    end
+
+    self._delegates = {}
+
+    self._toggles = {}
+
+    self._activeToggles = {}
+end
+
+ToggleGroupBase.__init = __init
+ToggleGroupBase.GetActiveToggleNum = GetActiveToggleNum
+ToggleGroupBase.OperateToggle = OperateToggle
+ToggleGroupBase.RegisterToggle = RegisterToggle
+ToggleGroupBase.GetToggles = GetToggles
+ToggleGroupBase.GetActiveToggles = GetActiveToggles
+ToggleGroupBase.__delete = __delete
+
+return ToggleGroupBase
